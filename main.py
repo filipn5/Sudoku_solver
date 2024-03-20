b3eeb85661df3b9ce8a7f1426204f1bee48f3d07
Antoni_Iwanowski <75222159+AntekIwa@users.noreply.github.com>
2024-03-20 21:10:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ def solve():
         for j in range(9):
             if board[i][j] == "0":
                 czy = True
-                for c in range(1, 9):
+                for c in range(1, 10):
                     if check(i, j, c):
+                        print(i,j,c)
                         board[i][j] = str(c)
+                        print(board[i][j])
                         if solve():
                             return True
                         board[i][j] = "0"
@@ -33,7 +35,6 @@ def solve():
 def fillRC():
     for i in range(9):
         for j in range(9):
-           # print(i, j, ((i%3) * 3) + (j % 3))
             rows[i][int(board[i][j])] = True
             columns[j][int(board[i][j])] = True
             squares[((i%3) * 3) + (j % 3)][int(board[i][j])] = True
@@ -46,9 +47,12 @@ def run():
     columns = [[False] * size] * size  # |||
     squares = [[False] * size] * size
     fillRC()
+    for i in range(9):
+        print(rows[i], columns[i], squares[i])
     if solve():
         communication.display_board(board)
     else:
         print("Wrong board")
+
 if __name__ == '__main__':
     run()
