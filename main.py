13648489fec256e4b3c9c7d140f27fa0dbb65418
Antoni_Iwanowski <75222159+AntekIwa@users.noreply.github.com>
2024-03-21 12:34:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-#TODO naprwaic solve - ranodmow wstawia true w rowsach
 import communication
 
 size = 10
 
 
 def check(row, col, candidat):
-    sqId = ((row//3) * 3) + (col // 3)
+    sqId = ((row // 3) * 3) + (col // 3)
     if not rows[row][candidat] and not columns[col][candidat] and not squares[sqId][candidat]:
         rows[row][candidat] = True
         columns[col][candidat] = True
@@ -15,14 +14,9 @@ def check(row, col, candidat):
 
 
 def solve():
-    communication.display_board(board)
-    print()
     czy = False
     for i in range(9):
         for j in range(9):
-            if(i == 0 and j == 3):
-                print(rows[i], columns[j])
-                print(check(i,j, 9))
             if board[i][j] == 0:
                 czy = True
                 for c in range(1, 10):
@@ -33,7 +27,7 @@ def solve():
                         board[i][j] = 0
                         rows[i][c] = False
                         columns[j][c] = False
-                        squares[(i //3)*3 + (j // 3)][c] = False
+                        squares[(i // 3) * 3 + (j // 3)][c] = False
                 if board[i][j] == 0:
                     return False
     if czy:
@@ -46,21 +40,21 @@ def fillRC():
         for j in range(9):
             rows[i][board[i][j]] = True
             columns[j][board[i][j]] = True
-            squares[(i //3)*3 + (j // 3)][board[i][j]] = True
+            squares[(i // 3) * 3 + (j // 3)][board[i][j]] = True
 
 
 def run():
     global board, rows, columns, squares
     board = communication.get_board()
     rows = [[False] * size for _ in range(size)]  # ---
-    columns = [[False] * size for _ in range(size)] # |||
+    columns = [[False] * size for _ in range(size)]  # |||
     squares = [[False] * size for _ in range(size)]
     fillRC()
-    print(rows[0])
-    czy = solve()
-    print("zostalo")
-    communication.display_board(board)
-    print(czy)
+    if solve():
+        communication.display_board(board)
+    else:
+        print("Wrong Board!!!")
+
 
 if __name__ == '__main__':
     run()
